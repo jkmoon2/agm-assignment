91b5266bda0666c9c59bedbcf0fc3f7239e0db60
jkmoon2 <moonddol3412@msn.com>
2025-04-16 12:57:15 +0900

    Update: [안정화 버젼]

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
-// 관리자 대시보드
+// 관리자 대시보드 페이지 (관리자 로그인 후 들어갈 페이지)
 import AdminDashboard from "./pages/AdminDashboard";
 
-// Auth0 관련 컴포넌트
+// Auth0 관련 컴포넌트들
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
 import Profile from "./components/Profile";
@@ -15,15 +15,23 @@ import PrivateRoute from "./components/PrivateRoute";
 function App() {
   const { isAuthenticated, user } = useAuth0();
 
-  // 관리자 이메일
+  // 관리자 이메일이 아래와 같으면 관리자임
   const adminEmail = "moonddol3412@msn.com";
   const isAdmin = isAuthenticated && user && user.email === adminEmail;
 
   return (
     <Router>
-      <div style={{ padding: 20 }}>
-        {/* 상단 헤더: 오직 우측에 로그인/로그아웃 + 프로필 */}
-        <header style={{ display: "flex", justifyContent: "flex-end", alignItems: "center", marginBottom: "20px" }}>
+      {/* 전체 컨테이너에 padding과 중앙 정렬 적용 */}
+      <div style={{ padding: 20, textAlign: "center" }}>
+        {/* 상단 헤더 - 우측 정렬 (프로필 및 로그인/로그아웃 버튼) */}
+        <header
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            alignItems: "center",
+            marginBottom: "20px"
+          }}
+        >
           <Profile />
           <div style={{ marginLeft: 10 }}>
             {!isAuthenticated && <LoginButton />}
@@ -31,17 +39,17 @@ function App() {
           </div>
         </header>
 
-        {/* 관리자라면 "Admin Dashboard" 링크 우측 상단 표시 */}
+        {/* 관리자 로그인시 관리자 대시보드 링크 (우측 상단에 표시) */}
         {isAuthenticated && isAdmin && (
           <div style={{ textAlign: "right", marginBottom: 20 }}>
-            <Link to="/admin" style={{ fontSize: "16px" }}>
+            <Link to="/admin" style={{ fontSize: "16px", textDecoration: "none" }}>
               Admin Dashboard
             </Link>
           </div>
         )}
 
         <Routes>
-          {/* 관리자 대시보드 (PrivateRoute) */}
+          {/* 관리자 대시보드: 관리자만 접근할 수 있도록 PrivateRoute 적용 */}
           <Route
             path="/admin"
             element={
@@ -50,7 +58,7 @@ function App() {
               </PrivateRoute>
             }
           />
-          {/* 홈 화면: 중앙 정렬 */}
+          {/* 기본 홈 화면: 중앙 정렬된 콘텐츠 */}
           <Route
             path="/"
             element={
