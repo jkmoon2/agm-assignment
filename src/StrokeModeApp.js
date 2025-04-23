89e553855e0754dc8712b7f67819872d5165f4e2
jkmoon2 <moonddol3412@msn.com>
2025-04-23 11:19:11 +0900

    fix: remove unused component imports for CI build

--- src/StrokeModeApp.js
+++ src/StrokeModeApp.js
@@ -8,8 +8,7 @@ import * as XLSX from 'xlsx';
 
 // 분리된 컴포넌트들
 import { ControlPanel } from './components/ControlPanel';
-import { AllocationTable } from './components/AllocationTable';
-import { ResultTable } from './components/ResultTable';
+
 
 // 공통 스타일 & 헬퍼 함수 import
 import {
